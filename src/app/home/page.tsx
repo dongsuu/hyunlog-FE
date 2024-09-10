4d17d713887a82db97fa5e66ccd74b492b52f5c2
donghyunlee <suudh99@naver.com>
2024-09-10 23:36:11 +0900

    feat: Home Page 레이아웃 추가 (#HLG-14)

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,12 @@
+import { Profile } from "@/widgets/profile/ui/Profile";
+import { Gnb } from "@/widgets/gnb";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div className="grid grid-cols-4 grid-rows-3 w-full h-full">
+      <Gnb />
+      <Profile />
+      <div className="row-span-2 col-span-3">Recent Posts Area</div>
+    </div>
+  );
 }
